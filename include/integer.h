970f96ce52b257b42cd01fc8c46cb11e783c65b5
Duane Byer <duane.byer@gmail.com>
2015-12-23 19:00:37 -0500

    Added multiplication

---
+++ include/integer.h
@@ -0,0 +1,150 @@
+#ifndef __APRN_INTEGER_H_
+#define __APRN_INTEGER_H_
+
+#include <cstdint>
+#include <istream>
+#include <ostream>
+#include <vector>
+
+namespace aprn {
+
+  class Integer {
+
+    friend std::ostream& operator<<(std::ostream& os, Integer const& obj);
+    friend std::istream& operator>>(std::istream& is, Integer& obj);
+    
+    friend int signum(Integer const& val);
+    friend Integer[] divRem(Integer const& lhs, Integer const& rhs);
+    friend bool operator==(Integer const& lhs, Integer const& rhs);
+    friend bool operator<(Integer const& lhs, Integer const& rhs);
+
+  public:
+    
+    // TODO: unary plus operator
+    
+    Integer operator-() const;
+    Integer& negate();
+
+    Integer& operator++();
+    Integer& operator--();
+
+    Integer operator++(int) {
+      Integer result(*this);
+      operator++();
+      return result;
+    }
+    
+    Integer operator--(int) {
+      Integer result(*this);
+      operator--();
+      return result;
+    }
+
+    Integer& operator+=(Integer const& rhs);
+    Integer& operator*=(Integer const& rhs);
+    Integer& operator/=(Integer const& rhs);
+    Integer& operator%=(Integer const& rhs);
+
+    Integer& operator-=(Integer const& rhs) {
+      operator+=(-rhs());
+      return *this;
+    }
+
+  private:
+
+    // Implementation Details
+    // ----------------------
+    //   An Integer is represented by a vector of digits in complement form (similar
+    // to two's complement, but for an arbitrary power-of-two base). If the most
+    // significant digit is smaller than half of the base, the integer is positive,
+    // otherwise it is negative. If there are no digits at all, the integer is zero.
+    // To negate an integer, each digit must be replaced with its complement, and
+    // one must be added to the result.
+    //   Each integer is guaranteed to have a unique representation as a string of
+    // digits satisfying the following constraints (where b is the base).
+    //   1) The representation of '0' is the single digit 0. All other strings that
+    //      would evaluate to 0 are invalid.
+    //        invalid base-8 examples: '000', '70'
+    //        fixed base-8 examples:     '0',  '0'
+    //   2) If the most significant digit is a 0, then the second-most significant
+    //      digit must be larger or equal to b/2.
+    //        invalid base-8 examples: '03', '017', '023', '004', '0005'
+    //        fixed base-8 examples:    '3',  '17',  '23',  '04',   '05'
+    //   3) If the most significant digit is b-1, then the second-most significant
+    //      digit must be smaller than b/2.
+    //        invalid base-8 examples: '75', '763', '772', '7771'
+    //        fixed base-8 examples:    '5',  '63',  '72',   '71'
+    //
+    //   The type used for the digit can be any unsigned integer type.
+    
+    // The type used for the digits of an integer.
+    using Digit = std::uint32_t;
+    using DoubleDigit = std::uint64_t;
+    // This constant stores the maximum value a digit can have. This is
+    // equal to the base minus 1.
+    Digit static const MAX_DIGIT;
+    // This constant stores the particular value such that if the most
+    // significant digit is larger than this value, the integer is
+    // negative, and if it is smaller, then the integer is positive.
+    Digit static const CRITICAL_DIGIT;
+    // The string of digits that represents the integer.
+    std::vector<Digit> m_digits;
+    // The size type of the vector.
+    using SizeType = decltype(m_digits)::size_type;
+
+  };
+
+  int signum(Integer const& val) {
+    if (val.m_digits.size() == 1 && val.m_digits.front() == 0) {
+      return 0;
+    }
+    else {
+      return
+        (val.m_digits.back() < Integer::CRITICAL_DIGIT) -
+        (val.m_digits.back() > Integer::CRITICAL_DIGIT);
+    }
+  }
+  
+  std::ostream& operator<<(std::ostream os&, Integer const& obj);
+  std::istream& operator>>(std::istream& is, Integer& obj);
+
+  bool operator==(Integer const& lhs, Integer const& rhs);
+  bool operator<(Integer const& lhs, Integer const& rhs);
+
+  inline bool operator!=(Integer const& lhs, Integer const& rhs) {
+    return !operator==(lhs, rhs);
+  }
+  inline bool operator>(Integer const& lhs, Integer const& rhs) {
+    return operator<(rhs, lhs);
+  }
+  inline bool operator<=(Integer const& lhs, Integer const& rhs) {
+    return !operator>(lhs, rhs);
+  }
+  inline bool operator>=(Integer const& lhs, Integer const& rhs) {
+    return !operator<(lhs, rhs);
+  }
+
+  inline Integer operator+(Integer lhs, Integer const& rhs) {
+    lhs += rhs;
+    return lhs;
+  }
+  inline Integer operator-(Integer lhs, Integer const& rhs) {
+    lhs -= rhs;
+    return lhs;
+  }
+  inline Integer operator*(Integer lhs, Integer const& rhs) {
+    lhs *= rhs;
+    return lhs;
+  }
+  inline Integer operator/(Integer lhs, Integer const& rhs) {
+    lhs /= rhs;
+    return lhs;
+  }
+  inline Integer operator%(Integer lhs, Integer const& rhs) {
+    lhs %= rhs;
+    return lhs;
+  }
+
+}
+
+#endif
---
+++ src/integer.cpp
@@ -0,0 +1,268 @@
+#include "include/integer.h"
+
+#include <algorithm>
+#include <istream>
+#include <limits>
+#include <ostream>
+#include <string>
+#include <vector>
+
+using namespace aprn;
+
+const Digit Integer::MAX_DIGIT = std::numeric_limits<Digit>::max();
+const Digit Integer::CRITICAL_DIGIT = MAX_DIGIT / 2;
+
+void makeValid(Integer& val) {
+  bool validForm = false;
+  while(!validForm && val.m_digits.size() >= 2) {
+    if (
+        val.m_digits.last() == MAX_DIGIT &&
+        val.m_digits[m_digits.size() - 2] >= CRITICAL_DIGIT) {
+      valm_digits.pop_back();
+    }
+    else if (
+             val.m_digits.last() == 0 &&
+             val.m_digits[m_digits.size() - 2] < CRITICAL_DIGIT) {
+      val.m_digits.pop_back();
+    }
+    else {
+      validForm = true;
+    }
+  }
+}
+
+Integer Integer::operator-() const {
+  Integer result(*this);
+  return result.negate();
+}
+
+Integer& Integer::negate() {
+  for (SizeType i = 0; i < m_digits.size(); ++i) {
+    m_digits[i] = ~m_digits[i];
+  }
+  operator++();
+  return *this;      
+}
+
+Integer& Integer::operator++() {
+  bool hasCarry = false;
+  SizeType lastCarryIndex;
+  for (SizeType i = 0; i < m_digits.size(); ++i) {
+    hasCarry = (++m_digits[i] == 0);
+    lastCarryIndex = i;
+    if (!hasCarry) {
+      break;
+    }
+  }
+  if (lastCarryIndex == m_digits.size() - 1 && m_digits.back() == CRITICAL_DIGIT) {
+    m_digits.push_back(0);
+  }
+  if (
+      lastCarryIndex == m_digits.size() - 2 &&
+      m_digits.back() == MAX_DIGIT &&
+      m_digits[m_digits.size() - 2] == CRITICAL_DIGIT) {
+    m_digits.pop_back();
+  }
+  return *this;
+}
+
+Integer& Integer::operator--() {
+  bool hasBorrow = false;
+  SizeType lastBorrowIndex;
+  for (SizeType i = 0; i < m_digits.size(); ++i) {
+    hasBorrow = (--m_digits[i] == MAX_DIGIT);
+    lastBorrowIndex = i;
+    if (!hasBorrow) {
+      break;
+    }
+  }
+  if (lastBorrowIndex == m_digits.size() - 1 && m_digits.back() == CRITICAL_DIGIT - 1) {
+    m_digits.push_back(MAX_DIGIT);
+  }
+  if (
+      lastCarryIndex = m_digits.size() - 2 &&
+      m_digits.back() == 0 &&
+      m_digits[m_digits.size() - 2] == CRITICAL_DIGIT - 1) {
+    m_digits.pop_back();
+  }
+  return *this;
+}
+
+Integer& Integer::operator+=(Integer const& rhs) {
+  SizeType numDigits = std::max(m_digits.size(), rhs.m_digits.size());
+  bool lhsIsNegative = m_digits.last() >= CRITICAL_DIGIT;
+  bool rhsIsNegative = rhs.m_digits.last() >= CRITICAL_DIGIT;
+  bool hasCarry = false;
+  
+  m_digits.resize(numDigits, isNegative * MAX_DIGIT);
+  
+  for (SizeType i = 0; i < numDigits; ++i) {
+    Digit lhsDigit = m_digits[i];
+    Digit rhsDigit = i < rhs.m_digits.size() ? rhs.m_digits[i] : (rhsIsNegative * MAX_DIGIT)
+    Digit minDigit = std::min(m_digits[i], rhs.m_digits[i]);
+
+    m_digits[i] += rhs.m_digits[i] + hasCarry;
+    hasCarry = m_digits[i] < minDigit;
+  }
+  
+  if (hasCarry) {
+    m_digits.push_back(hasCarry + lhsIsNegative * MAX_DIGIT + rhsIsNegative * MAX_DIGIT);
+  }
+
+  makeValid(*this);
+
+  return *this;
+}
+
+Integer& operator*=(Integer const& rhs) {
+  Integer lhs(*this);
+  Digit carry = 0;
+  
+  m_digits.clear();
+  m_digits.push_back(0);
+  
+  for (SizeType i = 0; i < rhs.m_digits.size(); ++i) {
+    Digit rhsDigit = rhs.m_digits[i];
+    Integer nextSum;
+    nextSum.resize(i, 0);
+    carry = 0;
+    for (SizeType j = 0; j < lhs.m_digits.size(); ++j) {
+      Digit lhsDigit = lhs.m_digits[j];
+      DoubleDigit digitProduct = lhsDigit * rhsDigit + carry;
+      carry = reinterpret_cast<Digit*>(&digitProduct)[0];
+      nextSum.push_back(reinterpret_cast<Digit*>(&digitProduct)[1]);
+    }
+    nextSum.push_back(carry);
+    operator+=(nextSum);
+  }
+  
+  makeValid(*this);
+  
+  return *this;
+}
+
+Integer[] divRem(Integer const& lhs, Integer const& rhs) {
+  
+}
+
+bool operator==(Integer const& lhs, Integer const& rhs) {
+  if (lhs.m_digits.size() != rhs.m_digits.size()) {
+    return false;
+  }
+  for (Integer::SizeType i = 0; i < lhs.m_digits.size(); ++i) {
+    Integer::Digit lhsDigit = lhs.m_digits[i];
+    Integer::Digit rhsDigit = rhs.m_digits[i];
+    if (lhsDigit != rhsDigit) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool operator<(Integer const& lhs, Integer const& rhs) {
+  int lhsSignum = signum(lhs);
+  int rhsSignum = signum(rhs);
+  
+  if (lhs.m_digits.size() != rhs.m_digits.size()) {
+    return lhsSignum * lhs.m_digits.size() < rhsSignum * rhs.m_digits.size();
+  }
+  
+  Integer::Digit lhsSignDigit = lhs.m_digits.back();
+  Integer::Digit rhsSignDigit = rhs.m_digits.back();
+    
+  // The most significant digit is the sign digit, which is ordered in a
+  // different order than the rest of the digits. For example, if the sign
+  // digit was in base 8:   4 < 5 < 6 < 7 < 0 < 1 < 2 < 3.
+  if (lhsSignDigit != rhsSignDigit) {
+    // This complicated boolean expression is just comparing the sign digit
+    // based on the above ordering.
+    bool hasSameSign = (lhsSignum == rhsSignum);
+    return
+      (!hasSameSign && (lhsSignum < rhsSignum)) ||
+      (hasSameSign && (lhsSignDigit < rhsSignDigit));
+  }
+  
+  // All of the other digits are ordered in the expected order, which is:
+  // 0 < 1 < 2 ... . Here, the digits are iterated through from most
+  // significant to least significant.
+  for (Integer::SizeType i = lhs.m_digits.size() - 2; i >= 0; --i) {
+    Integer::Digit lhsDigit = lhs.m_digits[i];
+    Integer::Digit rhsDigit = rhs.m_digits[i];
+    if (lhsDigit != rhsDigit) {
+      return lhsDigit < rhsDigit;
+    }
+  }
+  
+  // If we made it to here, then every digit was equal, meaning that the
+  // left-hand side is not smaller than the right-hand side.
+  return false;
+}
+
+std::ostream& operator<<(std::ostream& os, Integer const& obj) {
+  int base = 10;
+  int sign = signum(obj);
+  Integer value = sign < 0 ? -obj : obj;
+  std::string digits = "0123456789abcdef";
+  
+  if (os.flags() & std::ios::uppercase) {
+    std::transform(digits.begin(), digits.end(), digits.begin(), ::toupper);
+  }
+  
+  switch (os.flags() & std::ios::basefield) {
+  case std::ios::dec:
+    base = 10;
+    break;
+  case std::ios::oct:
+    base = 8;
+    break;
+  case std::ios::hex:
+    base = 16;
+    break;
+  }
+  
+  if (os.flags() & std::ios::showpos) {
+    if (sign >= 0) {
+      os << '+';
+    }
+  }
+  if (sign < 0) {
+    os << '-';
+  }
+  
+  if (os.flags() & std::ios::showbase) {
+    if (base == 8) {
+      os << '0';
+    }
+    else if (base == 16) {
+      os << "0x";
+    }
+  }
+  
+  if (sign == 0) {
+    os << '0';
+  }
+  else {
+    std::string output = "";
+    while (true) {
+      if (value < base) {
+        output.push_back(digits[(int) value]);
+        break;
+      }
+      else {
+        Integer[] dr = divRem(value, base);
+        value = dr[0];
+        output.push_back(digits[(int) dr[1]]);
+      }
+    }
+    os << output;
+  }
+  
+  return os;
+}
+
+std::istream& operator>>(std::istream& is, Integer& obj) {
+  if (/* no valid integer */true) {
+    is.setstate(std::ios::failbit);
+  }
+  return is;
+}
